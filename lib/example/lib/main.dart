553f4c5fa0b65e0c5b6c28504bb07e02fdfc3659
Damonique Blake <thomas.2576@osu.edu>
2021-11-22 19:19:24 -0500

    Add example app

---
+++ lib/example/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:trakt_dart/trakt_dart.dart';
+
+void main() => runApp(const TraktDartApp());
+
+class TraktDartApp extends StatelessWidget {
+  const TraktDartApp({Key? key}) : super(key: key);
+
+  Future<List<TrendingMovie>> getTrendingMovies() {
+    // Replace with your clientId and clientId from Trakt API.
+    TraktManager.instance.initializeTraktMananager(
+        clientId: "clientId", clientSecret: "clientId");
+
+    return TraktManager.instance.getTrendingMovies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Trending Movies'),
+        ),
+        body: FutureBuilder<List<TrendingMovie>>(
+          future: getTrendingMovies(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const CircularProgressIndicator();
+            }
+            final trendingMovies = snapshot.data!;
+            return ListView.builder(
+              itemCount: trendingMovies.length,
+              itemBuilder: (context, index) {
+                final movie = trendingMovies[index];
+                return Text(movie.movie.title);
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
